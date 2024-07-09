d39be97a9f11c1031d562f3ac062840a1f7b5af8
OussamaAlouat <al317257@uji.es>
2024-07-09 12:12:54 +0200

    FIX: added modules

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProgressComponent } from './progress/progress.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { PagesComponent } from './pages.component';
+import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent,
+  ],
+  exports: [
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent,
+    DashboardComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SharedModule
+  ],
+})
+export class PagesModule { }
