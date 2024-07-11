dd565c9a367a15f2dbb5b6a03845147dde01f6c5
OussamaAlouat <al317257@uji.es>
2024-07-11 11:46:14 +0200

    FIX: added progress operating logic

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./progress.component.css']
 })
 export class ProgressComponent {
+  progress:number = 50;
 
+  get getPercentage(): string {
+    return `${this.progress}%`;
+  }
+
+  changeValue(value: number){
+    if(this.progress >= 100 && value >=0) {
+     return this.progress = 100;
+    }
+
+    if(this.progress <=0 && value < 0) {
+      return this.progress = 0
+    }
+
+    return this.progress += value;
+  }
 }
