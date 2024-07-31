640021d1761cc4c983d51e671fdae9a5ca89722d
OussamaAlouat <al317257@uji.es>
2024-07-31 12:43:28 +0200

    FIX: saved the current theme

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./account-settings.component.css']
 })
 export class AccountSettingsComponent {
+  public linkTheme = document.querySelector('#theme');
 
+  changeTheme(theme: string) {
+    const url = `./assets/css/colors/${theme}.css`;
+    this.linkTheme?.setAttribute('href', url);
+    localStorage.setItem('theme', theme);
+  }
 }
