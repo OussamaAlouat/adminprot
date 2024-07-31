bbd03153055c2bf69424f2cb179e0b4b0c4a507e
OussamaAlouat <al317257@uji.es>
2024-07-31 12:43:44 +0200

    FIX: load the saved theme at the init

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html'
 })
-export class PagesComponent {
 
+export class PagesComponent implements OnInit {
+  public linkTheme = document.querySelector('#theme');
+
+  ngOnInit(): void {
+    const theme = localStorage.getItem('theme');
+    const value = theme ? theme : 'default';
+    const url = `./assets/css/colors/${value}.css`;
+
+    console.log(url)
+
+    this.linkTheme?.setAttribute('href', url);
+  }
 }
