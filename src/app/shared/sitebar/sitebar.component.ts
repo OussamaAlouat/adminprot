81ae55b18ff76a8ac921723fc4faf8db8c8aff1e
OussamaAlouat <al317257@uji.es>
2024-08-01 12:16:06 +0200

    FIX: extract the menu to a service

--- src/app/shared/sitebar/sitebar.component.ts
+++ src/app/shared/sitebar/sitebar.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SidebarService } from 'src/app/services/sidebar.service';
 
 @Component({
   selector: 'app-sitebar',
   templateUrl: './sitebar.component.html',
   styleUrls: ['./sitebar.component.css']
 })
-export class SitebarComponent {
+export class SitebarComponent implements OnInit{
 
+  menuItems: any[] = []
+
+  constructor(private sideBarService: SidebarService){}
+
+
+  ngOnInit(): void {
+    this.menuItems = this.sideBarService.menu;
+    
+  }
 }
